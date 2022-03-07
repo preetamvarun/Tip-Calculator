dd1d36a7f1b0ec8f38a6d0fa4181cf315a2e53df
Preetam Varun <preetamarun99@gmail.com>
2022-03-07 09:18:10 +0530

    start calculating tip

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-const billAmount = document.getElementById("billIcon");
+const noOfPeople = document.getElementById('peopleIcon');
+const billAmount = document.getElementById('billIcon');
+const five = document.getElementById("five");
+const ten = document.getElementById("ten");
+const fifteen = document.getElementById("fifteen");
+const twentyFive = document.getElementById("twentyFive");
+const fifty = document.getElementById("fifty");
+
+let solve = () => {
+    console.log('function');
+}
+
+
+noOfPeople.addEventListener('keydown', solve);
+billAmount.addEventListener('keydown', solve);
+five.addEventListener('click',solve);
+ten.addEventListener('click',solve);
+fifteen.addEventListener('click',solve);
+twentyFive.addEventListener('click',solve);
+fifty.addEventListener('click',solve);
