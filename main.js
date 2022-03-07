0c72ca6e806fe8f85980b3ca388413a9b2599122
Preetam Varun <preetamarun99@gmail.com>
2022-03-07 11:59:53 +0530

    sucessfully reading user inputs for tip calculation through javascript

--- main.js
+++ main.js
@@ -5,16 +5,80 @@ const ten = document.getElementById("ten");
 const fifteen = document.getElementById("fifteen");
 const twentyFive = document.getElementById("twentyFive");
 const fifty = document.getElementById("fifty");
+const customPercentage = document.getElementById("custom-percentage");
+const num = Array.from(document.getElementsByClassName("num"));
+
+let tip = 0, people, bill;
 
 let solve = () => {
-    console.log('function');
+
+    if(!isNaN(billAmount.value) && !isNaN(noOfPeople.value) && (tip !== 0)){
+        bill = Number(billAmount.value);        
+        people = Number(noOfPeople.value); 
+    }
+
 }
 
+noOfPeople.addEventListener('keyup', () => solve());
+billAmount.addEventListener('keyup', () => solve());
 
-noOfPeople.addEventListener('keydown', solve);
-billAmount.addEventListener('keydown', solve);
-five.addEventListener('click',solve);
-ten.addEventListener('click',solve);
-fifteen.addEventListener('click',solve);
-twentyFive.addEventListener('click',solve);
-fifty.addEventListener('click',solve);
+customPercentage.addEventListener('keyup',() => {
+    num.forEach((n) => {
+        if(n.classList.contains("clickClass")) n.classList.remove("clickClass");
+    });
+    tip = Number(customPercentage.value);
+    solve();
+});
+
+let x = (value) => {
+
+    customPercentage.value = "";
+
+    tip = Number(value);
+
+    let str;
+
+    num.forEach((n) => {
+        str = n.textContent.slice(0,-1);
+        if(str != value){
+            if(n.classList.contains("clickClass")){
+                n.classList.remove("clickClass");
+            }
+        }
+    });
+
+}
+
+five.addEventListener('click',() =>{
+    five.classList.toggle('clickClass');
+    x("5");
+    solve();
+});
+
+ten.addEventListener('click',() => {
+    ten.classList.toggle('clickClass');
+    x("10");
+    solve();
+});
+
+fifteen.addEventListener('click',() => {
+    fifteen.classList.toggle('clickClass');
+    x("15");
+    solve();
+});
+
+twentyFive.addEventListener('click',() => {
+    twentyFive.classList.toggle('clickClass');
+    x("25");
+    solve();
+});
+
+fifty.addEventListener('click',() => {
+    fifty.classList.toggle('clickClass');
+    x("50");
+    solve();
+});
+
+
+
+
